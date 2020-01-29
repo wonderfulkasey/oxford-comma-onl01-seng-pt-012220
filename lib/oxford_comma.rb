312b19424e870e38a782ebc1746f3e4222af8b77
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-29 17:26:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,12 @@
 def oxford_comma(array)
 
-  if array.length == 2 
+    if array.length == 2 
     return "#{array[0]} and #{array[1]}"
     
+    elsif 2 < array.length
+    array[-1].insert(0, "and ")
     
-elsif 2 < array.length
-
-array[-1].insert(0, "and ")
-end
+    end
 
 array.join(", ")
 
